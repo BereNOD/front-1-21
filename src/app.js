c6c59585f6919c3f9fcfecec596e80ad89d0d005
smirnoffnew <smirnoffnew@gmail.com>
2020-02-18 17:17:46 +0200

    Init

---
+++ src/app.js
@@ -0,0 +1,31 @@
+
+import React from "react";
+import { hot } from 'react-hot-loader/root';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navigation from './components/Navigation';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+export default hot(App);
